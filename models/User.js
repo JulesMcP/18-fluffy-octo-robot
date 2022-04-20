d17f11fd3039c9fef1805e2c4f82d30645152f7a
JulesMcP <rogue427@yahoo.com>
2022-04-19 20:42:36 -0500

    adds models & date format, corrected file names & package.json

--- models/User.js
+++ models/User.js
@@ -1,15 +1,54 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const userSchema = newSchema({
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            //Must match a valid email address 
+            //validate: 
+        },
+        // Array of _id values referencing the Thought model
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        // Array of _id values referencing the User model (self-reference)
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false
     }
-})
+);
+
+// retrieves the length of the user's friends array field on query
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
+
+
+
