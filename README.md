12cfa6f32326096d76f8e059997327297243760b
JulesMcP <rogue427@yahoo.com>
2022-04-28 17:07:02 -0500

    adds readme & controllers

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # 18-fluffy-octo-robot
-Challenge 18: NoSQL Social Network API
+
+![License](https://img.shields.io/badge/License-MIT-blue)
+
+## Description
+
+This is a social media blog app. Users can post thoughts and add friends. Friends & users can react to posts.
+
+## Table of Contents
+* [GitHub Site](#GitHub)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Technologies](#technologies)
+* [Contributions](#contributions)
+* [Questions](#questions)
+* [License](#license)
+* [Screenshot](#screenshots)
+* [Walkthrough Video](#walkthrough)
+
+## GitHub Site
+
+[Click to go to GitHub site!](https://github.com/JulesMcP/18-fluffy-octo-robot)
+
+## Installation
+
+Clone the repo; run npm init; npm i express, npm i mongoose.
+
+## Usage
+
+For users to connect with friends online.
+
+
+## Technologies
+
+This app was created using Node, NPM, Express, Mongodb, Mongoose, Insomnia, and JavaScript.
+
+## Contributions
+
+To contribute to this project, reach out via the contact info below.
+
+## Questions
+
+You may direct any questions about the project to [JulesMcP](https://github.com/JulesMcP), via email: [rogue427@yahoo.com](mailto:rogue427@yahoo.com).
+
+## License
+
+This project was developed under the GNU license.
+[More information about MIT](https://opensource.org/licenses/MIT)
+
+## Screenshots
+
+
+## Walkthrough Video
+
+
--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,5 +1,82 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
-    
-} 
+     // get all thoughts
+    getAllThoughts({}, res) {
+        Thought.find({})
+            .populate({
+                path: 'reactions',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    // get thought by id
+    getThoughtById({ params }, res) {
+        Thought.findById({ _id: params.id })
+            .populate({
+                path: 'reactions',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // createThought
+    createThought({ body }, res) {
+        Thought.create(body)
+            .then(dbThoughtData=> res.json(dbThoughtData))
+            .catch(err => res.json(err));
+    },
+
+    // update Thought by id
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // delete User
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // delete Friend
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.id }, { $pull: { friends: params.friendId } }, { new: true })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    }
+};
+
+module.exports = thoughtController;
